95c5271eee1420d4d90818aef2668886bb7fe44c
e-sung <dev.esung@gmail.com>
2017-11-19 14:44:18 +0900

    Add Initializers on ALarmItemCell

--- RaspberryAlarm/AlarmList/AlarmItemCell.swift
+++ RaspberryAlarm/AlarmList/AlarmItemCell.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class AlarmItemCell: UITableViewCell {
-    var alarmItem:AlarmItem
+    var alarmItem:AlarmItem{
+        get{
+            return _alarmItem
+        }
+        set(newItem){
+            self._alarmItem = newItem
+            self.timeLB.text = DateFormatter.formatHHmmss(alarmTime: newItem.timeToWakeUp)
+            color(the: self.dayLabels, of: newItem.repeatDays)
+        }
+    }
     
-    private var dayLabels:[UILabel]
-
+    private var dayLabels:[UILabel]{
+        get{
+            var labelsToReturn:[UILabel] = []
+            for i in 1...7{
+                labelsToReturn.append(self.viewWithTag(i) as! UILabel)
+            }
+            return labelsToReturn
+        }
+    }
+    
     private var _alarmItem:AlarmItem!
     @IBOutlet private weak var timeLB:UILabel!
     @IBAction private func switchToggleHandler(_ sender:UISwitch){
     }
-
+    
     private func color(the dayLabels:[UILabel], of repeatingDays:[Day]){
+        for label in dayLabels{
+            if repeatingDays.contains(Day(rawValue: label.tag)!){
+                label.textColor = .green
+            }else{
+                label.textColor = .lightGray
+            }
+        }
+    }
+    
+}
+
+extension DateFormatter{
+    static func formatHHmmss(alarmTime:AlarmTime)->String{
+        let hour:String = alarmTime.hour<10 ? "0\(alarmTime.hour)" : "\(alarmTime.hour)"
+        let minute:String = alarmTime.minute<10 ? "0\(alarmTime.minute)" : "\(alarmTime.minute)"
+        return "\(hour):\(minute)"
     }
     
 }
